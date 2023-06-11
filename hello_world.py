192e7e9c3e6d66eb8296e27298bb4a5b87870646
SharonLam0829 <Sharonlampuirong0829@gmail.com>
2023-06-11 12:55:21 +0800

    Update hello_world.py
    add to main branch

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,13 @@
 def hello(to):
     return f"Hello{to}!"
 
+def goodbye(to):
+    return f"Good Bye {to}!"
+
 def main():
     user = input("Name:")
     print(hello(user))
+    print(goodbye(user))
 
 if __name__== '__main__':
     main()
